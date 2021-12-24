eb1628b4af66e9cbb528c0a151e277b8d2d8336d
wangwh <wangwh@qaics.com>
2021-12-24 17:50:09 +0800

    1224

--- pytest/index.py
+++ pytest/index.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import win32com
 import win32com.client
 from win32com.client import Dispatch, DispatchEx
@@ -10,7 +11,7 @@ wordMain = Dispatch('Word.Application')
 
 currrent_path = os.getcwd()
 
-wordMain.Visible = False
+wordMain.Visible = True
 # print(os.getcwd())
 docMain = wordMain.Documents.Open(currrent_path + "/paper_part1.docx")
 docMain.Activate()
@@ -20,18 +21,17 @@ wordMain.ActiveDocument.Revisions.AcceptAll()
 
 # 修改所有表格样式为三线表
 try:
-  for table in wordMain.ActiveDocument.Tables:
-    # print(type("three_line1"))
-    if(str(table.Style) == "three_line1"):
-        print('不需要修改' + str(table.Style))
-    else:
-        table.Style = "three_line1"
-        print('已修改')
+    for table in wordMain.ActiveDocument.Tables:
+        # print(type("three_line1"))
+        if(str(table.Style) == "three_line1"):
+            print('不需要修改:' + str(table.Style))
+        else:
+            table.Style = "three_line1"
+            print('修改成功')
 except:
-  print('An exception occurred')
+    print('An exception occurred')
 else:
-    print('修改完成')
-
+    print('表格样式无异常')
 
 
 """ docMain.Close(False)
@@ -43,10 +43,10 @@ sys.exit(0)  """
 
 word1 = win32com.client.DispatchEx('Word.Application')
 
-doc_header = word1.Documents.Open(currrent_path + "/header.docx")
+""" doc_header = word1.Documents.Open(currrent_path + "/header.docx")
 # 复制word文件的所有内容
 doc_header.Content.Copy()
-doc_header.Close()
+doc_header.Close() """
 
 """ selection = wordMain.Selection
 selection.HomeKey(6, 0)
@@ -69,7 +69,7 @@ doc_footer.Close()
 # doc1.Range().Select()
 # doc.myRange.Selection.Paste()
 
-selection = wordMain.Selection
+# selection = wordMain.Selection
 # selection.MoveRight(1, docMain.Content.End) # 将光标移动到文末，就这一步试了我两个多小时
 # 使用EndKey更快一些
 # selection.EndKey(6,0)
@@ -77,36 +77,82 @@ selection = wordMain.Selection
 # selection.Paste()
 
 
-# 在目录二字下，生成目录
-toc_count = docMain.TablesOfContents.Count  # 判断是否有无目录，如果数量是1则代表已经有目录了
-if toc_count == 0:
-    print('还没有目录')
-    for i, p in enumerate(docMain.Paragraphs):  # 遍历word中的内容
-        print('查找目录...' + str(i))
-        if '目录占位符' in p.Range.Text:  # 用于指定目录页面，看下面提示
-            print('正在生成目录...')
-            # p.Range.InsertBreak()
-            # p.Range.InsertParagraphAfter()  # 添加新的段落 （容易出现 被呼叫方拒绝接收呼叫）
-            # p.Range.InsertAfter("---")
-            placeholder = p.Range
+attempts = 0
+success = False
 
-            # placeholder.InsertBefore('\r\n')
+toc_count = 0
+ignoreOutline = False
+while attempts < 3 and not success:
+    try:
+        toc_count = docMain.TablesOfContents.Count  # 判断是否有无目录，如果数量是1则代表已经有目录了,会尝试三次
+        success = True
+        print('获取目录成功')
+        break
+    except Exception as e:
+        attempts += 1
+        print('获取目录失败 ' + str(attempts) + '次，继续尝试..')
+        # 暂停0.5秒
+        time.sleep(0.5)
+
+if not success:    
+    ignoreOutline = True
+    print('已忽略目录')
+    print(e)
+
+if toc_count == 0 and ignoreOutline == False:
+    print('还没有目录')
+    # for i, p in enumerate(docMain.Paragraphs):  # 遍历word中的内容
+    for i in range(40):
+        print('查找插入目录的段落...' + str(i))
+        tempParagraph = docMain.Paragraphs[i]
+        if '目录占位符' in tempParagraph.Range.Text:
+            print('正在生成目录...')   
+            placeholder = docMain.Paragraphs[i].Range            
             docMain.TablesOfContents.Add(Range=placeholder,
                                          UseHeadingStyles=True,
                                          UpperHeadingLevel=1,
                                          LowerHeadingLevel=3)  # 生成目录对象
-            # p.Range.InsertParagraphAfter()
-            # placeholder.InsertBreak()
+           
             # 跳出
             break
+        # 暂停 0.2秒
+        time.sleep(0.2)
 
-elif toc_count == 1:
+# placeholder.InsertBefore('\r\n')
+# p.Range.InsertParagraphAfter()
+# placeholder.InsertBreak()
+# p.Range.InsertBreak()
+# p.Range.InsertParagraphAfter()  # 添加新的段落 （容易出现 被呼叫方拒绝接收呼叫）
+# p.Range.InsertAfter("---")
+# p.Range.InsertBefore("---")
+
+""" elif toc_count == 1:
     print('已经有目录了')
     toc = docMain.TablesOfContents(1)
-    toc.Update()
+    toc.Update() """
 
+
+try:
+    for i, p in enumerate(docMain.Paragraphs):  # 遍历word中的内容
+        print('寻找分页符，再次查找目录...' + str(i))
+        if '分页占位符' in p.Range.Text:
+            # p.Range.Collapse()
+            print('正在添加分页符...')
+            p.Range.InsertBreak(7)
+            time.sleep(0.5)
+except Exception as e:
+    print('插入分页符失败')
+    print(e)
+
+
+if(ignoreOutline == False and toc_count > 0):
+
+    # 再更新一下目录，不然有时候没有...引导线
+    outline = docMain.TablesOfContents(1)
+    outline.Update()
 
 wordMain.ActiveDocument.Save()
+time.sleep(1)
+
 docMain.Close(False)
-# wordMain.Quit()
 wordMain.Application.Quit()
--- pytest/readme.md
+++ pytest/readme.md
@@ -32,3 +32,27 @@ myRange.InsertBefore(translate_results) myRange.InsertBefore('\r\n')
 
 ### Python 使用 win32com 模块对 word 文件进行操作
 https://www.cnblogs.com/zhuminghui/p/11765401.html
+
+
+for x in wb.paragraphs:
+​ x.text=x.text.replace(‘ABC’,‘DEF’)
+
+
+
+## Word 中的 WdBreakType (枚举)
+
+
+指定分隔符的类型。
+
+| 名称                       | 值  | 说明                                                               |
+|--------------------------|----|------------------------------------------------------------------|
+| wdColumnBreak            | 8  | 插入点处的分栏符。                                                        |
+| wdLineBreak              | 6  | 换行符。                                                             |
+| wdLineBreakClearLeft     | 9  | 换行符。                                                             |
+| wdLineBreakClearRight    | 10 | 换行符。                                                             |
+| wdPageBreak              | 7  | 插入点处的分页符。                                                        |
+| wdSectionBreakContinuous | 3  | 新节不包含相应分页符。                                                      |
+| wdSectionBreakEvenPage   | 4  | 使下一节从下一偶数页开始的分节符。 如果分节符落入偶数页，则 Word 将下一奇数页留为空白。                  |
+| wdSectionBreakNextPage   | 2  | 分节符在下一页。                                                         |
+| wdSectionBreakOddPage    | 5  | 使下一节从下一奇数页开始的分节符。 如果分节符落入奇数页，则 Word 将下一偶数页留为空白。                  |
+| wdTextWrappingBreak      | 11 | 结束当前行，并强制文字在图片、表格或其他项目的下方继续。 文字将在下一个空行（且该空行不包含与左边距或右边距对齐的表格）上继续。 |
